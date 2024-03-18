c0a94ce7197ba58ffe6d36997cf84d93512a13c9
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2024-03-18 00:48:20 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -4,7 +4,7 @@
  * session persistence, api calls, and more.
  * */
 const Alexa = require('ask-sdk-core');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
 const DirecTVPauseIntentHandler = {
     canHandle(handlerInput) {
